c926a3743e7f35d574d219ffcd97df2d86f20ff7
Ben <bflatau@gmail.com>
2020-03-22 00:58:07 -0700

    started laying out grid

--- codeart_frontend/src/components/InputWall/InputWall.js
+++ codeart_frontend/src/components/InputWall/InputWall.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './styles.scss';
 import { Helmet } from 'react-helmet';
-import { storyPeople } from '../../constants/appData/people';
+import { inputBoardNumber } from '../../constants/appData/inputBoard';
 import { Redirect} from 'react-router-dom';
 
 
@@ -25,24 +25,56 @@ class InputWall extends React.Component {
 	
     componentDidMount() {
       console.log('loaded yo!');
-	}
+    }
+    
+
+    createInputGrid = () => {
+        let table = []
+    
+        for (let i = 0; i < inputBoardNumber; i++) {
+          
+            table.push(
+                <div 
+                    key={i+1} 
+                    onClick={()=>alert('you clicked ' + (i + 1))} 
+                    className= {(i === 20) ? 'input-button-active' : 'input-button'}
+                >
+
+                    {i +1} 
+
+                </div>
+            )
+        } 
+        return table
+      }
 	
 
 	
 	render () {
-		return (
-			  <div className="person-content-container">
-          <Helmet>
-            <title>Oral History</title>
-            <meta
-              name="Person"
-              content="person's name maybe?"
-            />
-          </Helmet>
 
-          <div> sup now</div>
-        </div>
-    
+		return (
+
+		    <div className="person-content-container">
+                <Helmet>
+                    <title>Oral History</title>
+                    <meta
+                    name="Person"
+                    content="person's name maybe?"
+                    />
+                </Helmet>
+
+                <div className='input-container'>
+                    <div className='input-board'>
+                       {this.createInputGrid()}
+                    </div> 
+                </div>
+
+
+
+
+
+
+            </div>
 		);
 	}
 }
---
+++ codeart_frontend/src/constants/appData/inputBoard.js
@@ -0,0 +1,2 @@
+
+export const inputBoardNumber = 50;
