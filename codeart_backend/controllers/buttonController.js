92a2c6331b2a43f9e62252191fd1dac9f4b22279
Ben <bflatau@gmail.com>
2020-03-26 23:30:13 -0700

    starting to mess with arrays...

--- codeart_backend/controllers/buttonController.js
+++ codeart_backend/controllers/buttonController.js
@@ -1,15 +1,40 @@
 const boardLength = 120;
 
 const games = [
-  { gameNumber: 1, numberOfKeys: 10 },
-  { gameNumber: 2, numberOfKeys: 14 },
-  { gameNumber: 3, numberOfKeys: 13 },
+  { gameNumber: 0, winningKeys: [ 1, 10, 41, 50 ] },
+  { gameNumber: 1, winningKeys: [ 1, 2, 3, 4, 5 ] },
+  { gameNumber: 2, winningKeys: [ 25 ] },
 ]
 
-////START FUNCTIONS/////
+
+
+///FUNCTIONS////
+
+function getRandom(arr, n) {
+  var result = new Array(n),
+      len = arr.length,
+      taken = new Array(len);
+  if (n > len)
+      throw new RangeError("getRandom: more elements taken than available");
+  while (n--) {
+      var x = Math.floor(Math.random() * len);
+      result[n] = arr[x in taken ? taken[x] : x];
+      taken[x] = --len in taken ? taken[len] : len;
+  }
+  return result;
+}
+
+
+console.log(getRandom([1,2,3,4], 2));
+
+
+
+
+
+////CONTROLLERS/////
 
 exports.getKeyQuantity = (req,res) => {
-  res.json({'data': games[req.params.gameNumber].numberOfKeys});
+  res.json({'data': games[req.params.gameNumber].winningKeys.length});
 }
 
 
@@ -18,9 +43,20 @@ exports.getKeyQuantity = (req,res) => {
 exports.handleKeyOn = (req, res) => {
   
   function generateResponseArray(){
+    const buttonInt = parseInt(req.params.buttonNumber);
+    const gameInt = parseInt(req.params.gameNumber);
     const responseArray = [];
+
+    //i values translate to direct board numbers (start at 1 NOT 0!)
     for (let i = 0; i < boardLength; i++){
-        responseArray.push(i + parseInt(req.params.buttonNumber));
+
+      if (i === 11 && i === buttonInt && gameInt === 1 ) {
+        responseArray.push('X')
+      }
+      else {
+        responseArray.push(1);
+      }
+       
     }
     return responseArray;
   }
@@ -29,6 +65,7 @@ exports.handleKeyOn = (req, res) => {
  
       res.json({'data': responseValue});
       console.log(req.params.buttonNumber);
+      console.log(req.params.gameNumber);
 };
 
 
@@ -37,7 +74,7 @@ exports.handleKeyOff = (req, res) => {
   function generateResponseArray(){
     const responseArray = [];
     for (let i = 0; i < boardLength; i++){
-        responseArray.push(i + parseInt(req.params.buttonNumber));
+        responseArray.push(0);
     }
     return responseArray;
   }
