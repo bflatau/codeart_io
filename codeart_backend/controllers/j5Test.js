4f1eac7f0ba78977bd6a037849bd12ce0fc65eb1
Ben <ben@ben.com>
2020-04-02 10:23:33 +0100

    got multiple buttons working!

---
+++ codeart_backend/controllers/j5Test.js
@@ -0,0 +1,69 @@
+// var five = require("johnny-five"),
+//     board = new five.Board();
+
+// board.on("ready", function() {
+//   // Create an Led on pin 13
+//   var led = new five.Led(13);
+
+//   // Strobe the pin on/off, defaults to 100ms phases
+//   led.strobe();
+// });
+
+var five = require("johnny-five"),
+  board, button;
+
+board = new five.Board();
+
+board.on("ready", function() {
+
+  // Create a new `button` hardware instance.
+  // This example allows the button module to
+  // create a completely default instance
+
+  // The "shared property" interface, allows
+  // writing a more succint initialization,
+  // as it's effectively a short hand for:
+  //
+  // var buttons = new five.Buttons([
+  //   { pin: 2, invert: true },
+  //   { pin: 3, invert: true },
+  //   { pin: 4, invert: true },
+  //   { pin: 5, invert: true },
+  //   { pin: 6, invert: true },
+  // });
+  //
+
+
+  buttons = new five.Buttons({
+      pins: [12, 11, 10],
+      holdtime: 2000
+    //   pin: 50
+    //   invert: true
+  });
+
+  // Inject the `button` hardware into
+  // the Repl instance's context;
+  // allows direct command line access
+//   board.repl.inject({
+//     button: button
+//   });
+
+  // Button Event API
+  
+  // "down" the button is pressed
+//   button.on("down", function() {
+//     console.log("down");
+//   });
+
+  // "hold" the button is pressed for specified time.
+  //        defaults to 500ms (1/2 second)
+  //        set
+  buttons.on("hold", function(button) {
+    console.log("this button is on", button.pin);
+  });
+
+  // "up" the button is released
+//   button.on("up", function() {
+//     console.log("up");
+//   });
+});
