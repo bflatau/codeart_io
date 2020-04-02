28f427157ce6df00ec1d7f39230c0b07575eccab
Ben <ben@ben.com>
2020-04-02 04:37:41 +0100

    got sockets working better

--- codeart_frontend/V2/src/components/SocketTest/SocketTest.js
+++ codeart_frontend/V2/src/components/SocketTest/SocketTest.js
@@ -1,25 +1,43 @@
-import React from 'react';
+import React, { Component } from "react";
+import socketIOClient from "socket.io-client";
 import './socket-test.css';
 
-function SocketTest() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class SocketTest extends Component {
+  constructor() {
+    super();
+    this.state = {
+      endpoint: "http://raspberrypi:8090",
+      color: 'white',
+    };
+  }
 
+  componentDidMount() {
+    // on initial mount, set all websocket stuff //
+    const socket = socketIOClient(this.state.endpoint);
+    socket.on('change color', (col) => {
+      document.body.style.backgroundColor = col
+    })
+  }
+
+  // sending sockets
+  send = () => {
+    const socket = socketIOClient(this.state.endpoint);
+    socket.emit('change color', this.state.color) // change 'red' to this.state.color
+  }
+  
+  // adding the function
+  setColor = (color) => {
+    this.setState({color})
+  }
+
+  render() {
+    return (
+      <div style={{ textAlign: "center" }}>
+        <button onClick={() => this.send() }>Change Color</button>
+        <button id="blue" onClick={() => this.setColor('blue')}>Blue</button>
+        <button id="red" onClick={() => this.setColor('red')}>Red</button>
+      </div>
+    )
+  }
+}
 export default SocketTest;
