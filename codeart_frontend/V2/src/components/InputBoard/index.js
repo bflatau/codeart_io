3ea90a1c2473d76d66d5c424891e3dd8bf1bd92b
Ben <bflatau@gmail.com>
2020-04-19 22:41:49 -0700

    added in symbols

--- codeart_frontend/V2/src/components/InputBoard/index.js
+++ codeart_frontend/V2/src/components/InputBoard/index.js
@@ -1,24 +1,33 @@
 import React, { Component } from "react";
 import './style.css';
 import InputBoardButton from '../InputBoardButton';
-import { TiPlus } from "react-icons/ti";
+import { TiTimesOutline, TiTimes, TiStarOutline, TiStar,  TiMediaStopOutline, TiMediaStop, TiMediaRecord, TiMediaRecordOutline } from "react-icons/ti";
 
 
 const gameBoardSymbolsColumns = [
-    1, 2, 3, 4, 5 , 6 , <TiPlus className = 'orange' /> , 8, 9 , 10,
-    11, 12, 13, 15, 15, 16, 17, 18, 19, 20,
-    21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
-    31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
-    41, 42, 43, 44, 45, 46, 47, 48, 49, 50,
-    51, 52, 53, 54, 55, 56, 57, 58, 59, 60
+    <TiStarOutline className='black'/>, <TiTimesOutline className = 'red' /> , <TiMediaStopOutline className='black' />, <TiMediaRecord className='blue' />, <TiStarOutline className='black'/> , <TiTimes className='black' /> , <TiMediaStop className = 'purple' /> , <TiMediaStopOutline className='orange' />, <TiTimesOutline className='cyan'/> , <TiMediaStopOutline className="black"/>,
+   
+    <TiTimesOutline className='black'/>, <TiTimesOutline className='red'/>, <TiMediaStop className='black'/>, <TiMediaRecord className='black'/>, <TiStar className='blue'/>, <TiMediaStopOutline className='black'/>, <TiTimes className='purple'/>, <TiMediaStopOutline className='orange'/>, <TiTimesOutline className='cyan'/>, <TiMediaRecordOutline className='black'/> ,
+    
+    <TiTimesOutline className='black'/>, <TiMediaStopOutline className='red'/>, <TiMediaRecord className='green'/>, <TiTimes className='black'/>, <TiMediaRecord className='blue'/>, <TiMediaStopOutline className='purple'/>, <TiStar className='black'/>, <TiTimes className='black'/>, <TiMediaStopOutline className='cyan'/>, <TiStarOutline className='black'/>,
+    
+    <TiTimes className='red'/>, <TiMediaStopOutline className='black'/>, <TiMediaRecordOutline className='green'/>, <TiStar className='blue'/>, <TiMediaStopOutline className='black'/>, <TiTimes className='purple'/>, <TiStarOutline className='cyan'/>, <TiStarOutline className='black'/>, <TiMediaStop className='magenta'/>, <TiTimes className='black'/>,
+    
+    <TiStarOutline className='red'/>, <TiMediaStop className='black'/>, <TiTimesOutline className='green'/>, <TiMediaRecordOutline className='yellow'/>, <TiMediaStopOutline className='blue'/>, <TiStar className='black'/>, <TiTimes className='purple'/>, <TiMediaRecordOutline className='cyan'/>, <TiStarOutline className='magenta'/>, <TiMediaStopOutline className='black'/>,
+    
+    <TiMediaRecordOutline className='red'/>, <TiMediaStop className='black'/>, <TiTimesOutline className='green'/>, <TiMediaRecordOutline className='yellow'/>, <TiMediaStopOutline className='black'/>, <TiStar className='black'/>, <TiTimesOutline className='purple'/>, <TiMediaRecord className='cyan'/>, <TiStarOutline className='black'/>, <TiMediaStopOutline className='black'/>
 ];
 
 const gameBoardSymbolsRows = [
-    1, 2, 3, 4, 5 , 6 , 7 , 8, 9 , 10, 11, 
-    12, 13, 15, 15, 16, 17, 18, 19, 20, 21, 22, 
-    23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 
-    34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 
-    45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 
+    <TiMediaStop className='black'/>, <TiMediaRecord className='black'/>,  <TiMediaRecord className='black'/>, <TiStarOutline className='black'/>, <TiTimesOutline className = 'blue' />, <TiMediaRecord className='black'/> , <TiMediaStopOutline className='black'/>  , <TiMediaRecordOutline className='black'/> , <TiStarOutline className='black'/> , <TiStarOutline className='black'/>, <TiTimesOutline className='black'/>, 
+    
+    <TiStarOutline className='green'/>, <TiTimesOutline className='green'/>, <TiMediaStopOutline className='green'/>, <TiMediaRecordOutline className='black'/>, <TiMediaStopOutline className='black'/>, <TiStarOutline className='black'/>, <TiStarOutline className='purple'/>, <TiStarOutline className='black'/>, <TiMediaStop className='orange'/>, <TiTimes className='orange'/>, <TiStar className='orange'/>, 
+    
+    <TiMediaRecordOutline className='black'/>, <TiMediaRecord className='red'/>, <TiStar className='black'/>, <TiMediaStopOutline className='black'/>, <TiMediaRecordOutline className='blue'/>, <TiMediaStop className='black'/>, <TiMediaRecordOutline className='black'/>, <TiMediaRecord className='cyan'/>, <TiStarOutline className='cyan'/>, <TiStar className='magenta'/>, <TiMediaRecordOutline className='magenta'/>, 
+    
+    <TiMediaStop className='yellow'/>, <TiTimesOutline className='yellow'/>, <TiTimes className='yellow'/>, <TiMediaRecord className='yellow'/>, <TiTimesOutline className='blue'/>, <TiMediaRecordOutline className='black'/>, <TiStarOutline className='purple'/>, <TiMediaStop className='cyan'/>, <TiStar className='black'/>, <TiMediaStopOutline className='black'/>, <TiTimes className='black'/>, 
+    
+    <TiStarOutline className='black'/>, <TiMediaStopOutline className='black'/>, <TiStarOutline className='black'/>, <TiMediaStopOutline className='black'/>, <TiStarOutline className='black'/>, <TiMediaRecordOutline className='black'/>, <TiMediaStopOutline className='black'/>, <TiStar className='black'/>, <TiTimesOutline className='black'/>, <TiMediaRecordOutline className='magenta'/>, <TiStarOutline className='magenta'/>, 
 ];
 
 class InputBoard extends Component {
