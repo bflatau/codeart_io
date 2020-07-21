1064836a69eea6c183bcab415970f87b079faa7a
Ben <bflatau@gmail.com>
2020-07-20 23:44:12 -0700

    added everything to webpack and whatnot

---
+++ codeart_frontend/V3/src/index.js
@@ -0,0 +1,4 @@
+import "./styles.scss";
+import threecode from './js/threecode';
+
+console.log("hello stupid!");
---
+++ codeart_frontend/V3/webpack.config.js
@@ -0,0 +1,39 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const config = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 3000,
+    // hot: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      },
+      {
+        test: /\.svg$/,
+        use: 'file-loader'
+      }
+    ]
+  }
+};
+
+module.exports = config;
