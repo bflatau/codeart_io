59843fdef774cabff179e170aa825b174715392e
ben <jelly@jelly.com>
2020-08-25 22:58:14 -0700

    turned damping back on

--- codeart_frontend/V3/src/js/lib/customOrbitControls.js
+++ codeart_frontend/V3/src/js/lib/customOrbitControls.js
@@ -60,7 +60,7 @@ var OrbitControls = function ( object, domElement ) {
 
 	// Set to true to enable damping (inertia)
 	// If damping is enabled, you must call controls.update() in your animation loop
-	this.enableDamping = false;
+	this.enableDamping = true;
 	this.dampingFactor = 0.05;
 
 	// This option actually enables dollying in and out; left as "zoom" for backwards compatibility.
