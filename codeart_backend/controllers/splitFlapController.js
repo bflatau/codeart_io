bad0e43f8d334958ba62feea56bc36849d2e6ea7
Ben <bflatau@gmail.com>
2021-09-01 01:59:49 -0700

    have a semi-working path forward!

--- codeart_backend/controllers/splitFlapController.js
+++ codeart_backend/controllers/splitFlapController.js
@@ -1,10 +1,54 @@
-// const splitFlapModules = 108; // 6 x 18
+var XLSX = require('xlsx');
+const fs = require('fs');
 
-const flapRows = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18];
-const numberOfRows = 6;
+
+
+
+
+///SETUP XLSX READER
+var buffer = fs.readFileSync("../code_art.xlsx");
+var workbook = XLSX.read(buffer, {type:'buffer'});
+
+
+
+var keys = Object.keys(workbook.Sheets['game_2']);//GETS KEY VALUES OF EACH SHEET [A1, G4, E6]...etc
+// console.log(keys)
+
+keys.forEach((key) => {
+    console.log(workbook.Sheets['game_2'][key].v) ///GETS VALUES ASSOCIATED WITH EACH KEY [B, E, N]...etc
+})
+
+
+
+// B2 - S2
+// B3 - S3 
+// B4 - S4 
+// B5 - S5 
+// B6 - S6 
+// B7 - S7 
+
+
+const splitFlapModules = {  
+    
+    'B2': 1, 'C2': 2, 'D2': 3, 'E2': 0, 'F2': 0, 'G2': 0, 'H2': 0, 'I2': 0, 'J2': 0, 'K2': 0, 'L2': 0, 'M2': 0, 'N2': 0, 'O2': 0, 'P2': 0, 'Q2': 4, 'R2': 5, 'S2': 6,
+    
+
+
+};
+
+console.log(splitFlapModules['G2'], 'hello')
+
+let benTest = [];
+
+for (const value in splitFlapModules) {
+
+    benTest.push(splitFlapModules[value])
+  }
+
+  console.log(benTest)
 
 const flaps = [
-    'black',
+    0, //BLACK
     'J',
     'B',
     'M',
@@ -16,9 +60,9 @@ const flaps = [
     'E',
     'N',
     'O',
-    'yellow',
+    12, // yellow
     '*',
-    'green',
+    14, // green
     'G',
     'I',
     '%',
@@ -30,10 +74,10 @@ const flaps = [
     'W',
     'H',
     'Y',
-    'white',
+    26, // white
     'Q',
-    'red',
-    'orange',
+    28, // red
+    29, // orange
     '!',
     'T',
     'Z',
@@ -46,22 +90,32 @@ const flaps = [
     'X'
 ]
 
-const quotes = [
 
-
-]
-
-
-const games = [
-    {   winningBoxes: [ 1, 2, 3, 4],
-        quoteLetters: [] ///this should use the row/column coordinates 
-    },
-  
-];
 
 
 ///SPLIT FLAP FUNCTION
 
 //ACCEPTS AN ARRAY (ON BOXES) & TESTS IF GAME WINNING CONDITIONS ARE MET
 
-console.log(flaps.length);
+
+
+
+
+// var firstSheet = workbook.SheetNames[0];
+
+// console.log(firstSheet) /// outputs sheet name
+
+// console.log(workbook.Sheets['game_1'])
+
+
+// var first_sheet_name = workbook.SheetNames[0];
+// var address_of_cell = 'A1';
+
+// /* Get worksheet */
+// var worksheet = workbook.Sheets[first_sheet_name];
+
+// /* Find desired cell */
+// var desired_cell = worksheet[address_of_cell];
+
+// /* Get the value */
+// var desired_value = (desired_cell ? desired_cell.v : undefined);
