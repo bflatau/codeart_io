2663b3ef6d785e13ae4cef50cbfc2e4b23c1998d
Ben Flatau <bflatau@gmail.com>
2022-06-09 00:01:49 -0700

    got embeddings working locally..

--- embeddings_api/embeddings_generate.py
+++ embeddings_api/embeddings_generate.py
@@ -1,31 +1,18 @@
 import os
 import openai
-openai.organization = "YOUR_ORG_ID"
-openai.api_key = os.getenv("OPENAI_API_KEY")
-# openai.Model.list()
-
-
 import pandas as pd
 
-df = pd.read_csv('input/Reviews.csv', index_col=0)
-df = df[['Time', 'ProductId', 'UserId', 'Score', 'Summary', 'Text']]
-df = df.dropna()
-df['combined'] = "Title: " + df.Summary.str.strip() + "; Content: " + df.Text.str.strip()
-df.head(2)
 
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
-# subsample to 1k most recent reviews and remove samples that are too long
-df = df.sort_values('Time').tail(1_100)
-df.drop('Time', axis=1, inplace=True)
+df = pd.read_csv('combined_season1-37.tsv',  sep='\t') #read file
+df = df[[ 'answer', 'question']] #name the columns we want
+df = df.dropna() #remove all the columns not identified above
+df['combined'] = "Question: " + df.question.str.strip() + "; Answer: " + df.answer.str.strip() #make superstring?
+#df.head(6) #this tests N rows to see if the data looks correct
 
-from transformers import GPT2TokenizerFast
-tokenizer = GPT2TokenizerFast.from_pretrained("gpt2")
-
-# remove reviews that are too long
-df['n_tokens'] = df.combined.apply(lambda x: len(tokenizer.encode(x)))
-df = df[df.n_tokens<2000].tail(1_000)
-len(df)
-
+df = df.tail(1000) # set df to the last N number of values to work with
+#len(df) #output length
 
 
 from openai.embeddings_utils import get_embedding
@@ -33,4 +20,4 @@ from openai.embeddings_utils import get_embedding
 # This will take just under 10 minutes
 df['babbage_similarity'] = df.combined.apply(lambda x: get_embedding(x, engine='text-similarity-babbage-001'))
 df['babbage_search'] = df.combined.apply(lambda x: get_embedding(x, engine='text-search-babbage-doc-001'))
-df.to_csv('output/embedded_1k_reviews.csv')
+df.to_csv('embedded_1k_reviews.csv')
--- embeddings_api/embeddings_search.py
+++ embeddings_api/embeddings_search.py
@@ -1,24 +1,26 @@
-
+import os
+import openai
 import pandas as pd
 import numpy as np
+from dotenv import load_dotenv, find_dotenv
+load_dotenv(find_dotenv())
 
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
-df = pd.read_csv('output/embedded_1k_reviews.csv')
+df = pd.read_csv('embedded_1k_reviews.csv')
 df['babbage_search'] = df.babbage_search.apply(eval).apply(np.array)
-
-
 
 from openai.embeddings_utils import get_embedding, cosine_similarity
 
 # search through the reviews for a specific product
-def search_reviews(df, product_description, n=3, pprint=True):
-    embedding = get_embedding(product_description, engine='text-search-babbage-query-001')
+def search_reviews(df, search_phrase, n=3, pprint=True):
+    embedding = get_embedding(search_phrase, engine='text-search-babbage-query-001')
     df['similarities'] = df.babbage_search.apply(lambda x: cosine_similarity(x, embedding))
 
-    res = df.sort_values('similarities', ascending=False).head(n).combined.str.replace('Title: ','').str.replace('; Content:', ': ')
+    res = df.sort_values('similarities', ascending=False).head(n).combined.str.replace('Question: ','').str.replace('; Answer:', ': ')
     if pprint:
         for r in res:
             print(r[:200])
             print()
     return res
-res = search_reviews(df, 'delicious beans', n=3)
+res = search_reviews(df, 'car', n=3)
