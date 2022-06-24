10d817def4085241170bd879f906e5483e8d0d8b
Ben Flatau <bflatau@gmail.com>
2022-06-24 10:14:26 -0700

    got it to read the whole giant file!

--- embeddings_api/embeddings_search.py
+++ embeddings_api/embeddings_search.py
@@ -9,14 +9,17 @@ load_dotenv(find_dotenv())
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 
-# df = pd.concat((chunk for chunk in pd.read_csv(csvname,chunksize=5000)))
+
+
+
+
 
 
 # search through the reviews for a specific product
 def search_reviews(search_phrase, n=3, pprint=True):
-    # df = pd.concat((chunk for chunk in pd.read_csv('./embeddings/embedded_season37_QA.csv', chunksize=1000, error_bad_lines=False, warn_bad_lines=True)))
-    df = pd.read_csv('./embeddings/embedded_season37_QA.csv')
-    df = df.tail(1000)
+    df= pd.read_csv('./embeddings/embedded_season37_QA.csv', error_bad_lines=False, engine="python")  # the number of rows per chunk
+
+
     df['curie_search'] = df.curie_search.apply(eval).apply(np.array)
     embedding = get_embedding(search_phrase, engine='text-search-curie-query-001')
     df['similarities'] = df.curie_search.apply(lambda x: cosine_similarity(x, embedding))
@@ -27,6 +30,23 @@ def search_reviews(search_phrase, n=3, pprint=True):
             print(r[:200])
             print()
     return res
+
+
+
+
+# search through the reviews for a specific product
+# def search_reviews(search_phrase, n=3, pprint=True):
+#     df = pd.read_csv('./embeddings/embedded_season37_QA.csv', nrows=1000) 
+#     df['curie_search'] = df.curie_search.apply(eval).apply(np.array)
+#     embedding = get_embedding(search_phrase, engine='text-search-curie-query-001')
+#     df['similarities'] = df.curie_search.apply(lambda x: cosine_similarity(x, embedding))
+
+#     res = df.sort_values('similarities', ascending=False).head(n).combined.str.replace('Question: ', '').str.replace('; Answer:', '^')
+#     if pprint:
+#         for r in res:
+#             print(r[:200])
+#             print()
+#     return res
 
 
 
@@ -42,4 +62,5 @@ def search_reviews(search_phrase, n=3, pprint=True):
 #             print(r[:200])
 #             print()
 #     return res
+
 
