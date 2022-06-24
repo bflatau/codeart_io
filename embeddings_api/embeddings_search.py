203b6b0a95cd7c9932196893559a2184cac92ac9
Ben Flatau <bflatau@gmail.com>
2022-06-24 01:44:23 -0700

    stashing for now...need to chunk out data

--- embeddings_api/embeddings_search.py
+++ embeddings_api/embeddings_search.py
@@ -9,15 +9,17 @@ load_dotenv(find_dotenv())
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 
-
+# df = pd.concat((chunk for chunk in pd.read_csv(csvname,chunksize=5000)))
 
 
 # search through the reviews for a specific product
 def search_reviews(search_phrase, n=3, pprint=True):
-    df = pd.read_csv('embedded_1k_reviews_QandA.csv')
-    df['babbage_search'] = df.babbage_search.apply(eval).apply(np.array)
-    embedding = get_embedding(search_phrase, engine='text-search-babbage-query-001')
-    df['similarities'] = df.babbage_search.apply(lambda x: cosine_similarity(x, embedding))
+    # df = pd.concat((chunk for chunk in pd.read_csv('./embeddings/embedded_season37_QA.csv', chunksize=1000, error_bad_lines=False, warn_bad_lines=True)))
+    df = pd.read_csv('./embeddings/embedded_season37_QA.csv')
+    df = df.tail(1000)
+    df['curie_search'] = df.curie_search.apply(eval).apply(np.array)
+    embedding = get_embedding(search_phrase, engine='text-search-curie-query-001')
+    df['similarities'] = df.curie_search.apply(lambda x: cosine_similarity(x, embedding))
 
     res = df.sort_values('similarities', ascending=False).head(n).combined.str.replace('Question: ', '').str.replace('; Answer:', '^')
     if pprint:
@@ -26,4 +28,18 @@ def search_reviews(search_phrase, n=3, pprint=True):
             print()
     return res
 
+
+
+# def search_reviews(search_phrase, n=3, pprint=True):
+#     df = pd.read_csv('embedded_1k_reviews_QandA.csv')
+#     df['babbage_search'] = df.babbage_search.apply(eval).apply(np.array)
+#     embedding = get_embedding(search_phrase, engine='text-search-babbage-query-001')
+#     df['similarities'] = df.babbage_search.apply(lambda x: cosine_similarity(x, embedding))
+
+#     res = df.sort_values('similarities', ascending=False).head(n).combined.str.replace('Question: ', '').str.replace('; Answer:', '^')
+#     if pprint:
+#         for r in res:
+#             print(r[:200])
+#             print()
+#     return res
 
