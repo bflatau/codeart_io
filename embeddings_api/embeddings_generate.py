116a65d74f7e902d02c4c3b2212db89cc8a8054b
Scott Bezek <scottbez1@gmail.com>
2022-06-28 10:55:46 -0700

    Filter before generating embeddings

--- embeddings_api/embeddings_generate.py
+++ embeddings_api/embeddings_generate.py
@@ -5,9 +5,18 @@ import pandas as pd
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
+ALLOWED_REGEX = r'[a-zA-Z\$\*\%\&\@\!\?\#\'\,\" ]*'
+
 df = pd.read_csv('combined_season1-37.tsv',  sep='\t') #read file
 df = df[[ 'answer', 'question']] #name the columns we want
 df = df.dropna() #remove all the columns not identified above
+print(df)
+
+# Filter out embeddings with characters we can't display
+df = df[df.apply(lambda row: (re.fullmatch(ALLOWED_REGEX, row['answer']) is not None) and (re.fullmatch(ALLOWED_REGEX, row['question']) is not None), axis=1)]
+print("FILTERED")
+print(df)
+
 df['combined'] = "Question: " + df.question.str.strip() + "; Answer: " + df.answer.str.strip() #make superstring?
 #df.head(6) #this tests N rows to see if the data looks correct
 
